56e4d11ab77d5c5ecc67cd13e71cd4b621ff3e34
jkloster7 <jakekloster@yahoo.com>
2020-02-28 16:49:51 -0600

    First commit

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php include('includes/header.php'); ?>
 
     <div class="container">
-        <h1>Login System</h1>
-        <p>This is the home page for the login system. This is just a template, once it is complete it can be used for multiple projects.</p>
+        <h1>Welcome to the Grocery App</h1>
+        <p>This is the new and improved app.</p>
     </div>
     
 <?php include('includes/footer.php'); ?>
